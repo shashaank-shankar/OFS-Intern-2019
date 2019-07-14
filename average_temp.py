2eb484ca89d2c84fb0b9bac786655c9507a53ad9
Shashaank Shankar <Shashaank@Shashaanks-MacBook-Air.local>
2019-07-14 17:47:33 -0400

    created  select method

--- average_temp.py
+++ average_temp.py
@@ -1,9 +1,22 @@
-# fix temp converter
-# import TempConverter
 import mysql.connector
 
 IOTdb = mysql.connector.connect(
   host="iotplatform.c66jtrv2jovs.us-east-1.rds.amazonaws.com",
   user="iotplatformusr",
-  passwd="iotplatformpwd"
+  passwd="iotplatformpwd",
+  database ="temperature_data_logger"
 )
+
+cursorObj = IOTdb.cursor()
+
+# 'columns' is what columns you want to select
+# 'tablename' is the name of the table
+def selectTable (columns, tablename):
+    cursorObj.execute(
+        "select " + columns + " from " + tablename
+    )
+    rows = cursorObj.fetchall()
+    for row in rows:
+        print(row)
+
+selectTable("TEMPERATURE_READING", "RECORD_TEMPERATURE")
