a21c59320185fe212239f28859ef7bd7184cffca
Shashaank Shankar <Shashaank@Shashaanks-MacBook-Air.local>
2019-07-14 19:27:09 -0400

    save all

--- TempConverter.py
+++ TempConverter.py
@@ -1,26 +1,29 @@
-print("\nThis program converts Celsius and Farenheit temperatures.")
+#print("\nThis program converts Celsius and Farenheit temperatures.")
 
-def tempConvert (type):
-    if type == "C":
+def tempConvert (type, temperature):
+    if type.upper() == "C":
         # convert to C
-        new_temp = (temp_input - 32) * (5/9)
+        new_temp = (temperature - 32.0) * (5.0/9.0)
         new_temp = round(new_temp, 2)
-        print("\n" + str(temp_input) + "F is equal to " + str(new_temp) + "C.")
-    elif type == "F":
+        #print("\n" + str(temperature) + "F is equal to " + str(new_temp) + "C.")
+        return new_temp
+    elif type.upper() == "F":
          # convert to F
-        new_temp = (temp_input * (9/5)) + 32
-        new_temp = round(new_temp, 2)
-        print("\n" + str(temp_input) + "C is equal to " + str(new_temp) + "F.")
+        new_temp = (temperature * (9.0/5.0)) + 32.0
+        new_temp = round(new_temp, 2.0)
+        #print("\n" + str(temperature) + "C is equal to " + str(new_temp) + "F.")
+        return new_temp
 
-# repeats until 'C' or 'F' is entered
-while True:
-    type_input = input("\nEnter 'C' to convert to Celsius\nEnter 'F' to convert to Farenheit\nEnter 'E' to Exit: ")
-    type_input = type_input.upper()
-    if type_input == "C":
-        temp_input = float(input("\nEnter a temperature in Farenheit: "))
-        tempConvert("C")
-    elif type_input == "F":
-        temp_input = float(input("\nEnter a temperature in Celsius: "))
-        tempConvert("F")
-    elif type_input == "E":
-        break   
+
+# repeats until 'C' or 'F' or 'E' is entered
+# while True:
+#     type_input = input("\nEnter 'C' to convert to Celsius\nEnter 'F' to convert to Farenheit\nEnter 'E' to Exit: ")
+#     type_input = type_input.upper()
+#     if type_input == "C":
+#         temp_input = float(input("\nEnter a temperature in Farenheit: "))
+#         tempConvert("C")
+#     elif type_input == "F":
+#         temp_input = float(input("\nEnter a temperature in Celsius: "))
+#         tempConvert("F")
+#     elif type_input == "E":
+#         break   
--- average_temp.py
+++ average_temp.py
@@ -1,3 +1,4 @@
+import TempConverter
 import mysql.connector
 
 # connects file to database
@@ -12,6 +13,7 @@ cursorObj = IOTdb.cursor()
 
 # list of all temperatures recorded
 temperature_list_temp = []
+# list w/ isolated numbers
 temperature_list = []
 
 # 'columns' is what columns you want to select
@@ -28,6 +30,15 @@ def selectTable (columns, tablename):
         #print(row)
     IOTdb.commit()
 
+# isolates number from entry
+def temperaturelist_strip (list):
+    for value in list:
+        value = str(value)
+        value.strip()
+        #print(value)
+        temperature_list.append(value)
+
+# gets average of list
 def average (list):
     sum = 0
     count = 0
@@ -37,16 +48,26 @@ def average (list):
     average = round(sum/count, 2)
     return average
 
-def temperaturelist_strip (list):
-    for value in list:
-        value = str(value)
-        value.strip()
-        #print(value)
-        temperature_list.append(value)
+# converts temperature
+def tempConvert (type, temperature):
+    if type.upper() == "C":
+        # convert to C
+        new_temp = (temperature - 32.0) * (5.0/9.0)
+        new_temp = round(new_temp, 2)
+        #print("\n" + str(temperature) + "F is equal to " + str(new_temp) + "C.")
+        return new_temp
+    elif type.upper() == "F":
+         # convert to F
+        new_temp = (temperature * (9.0/5.0)) + 32.0
+        new_temp = round(new_temp, 2.0)
+        #print("\n" + str(temperature) + "C is equal to " + str(new_temp) + "F.")
+        return new_temp
 
+# adds all values to temperature_list_temp
 selectTable("*", "RECORD_TEMPERATURE")
 
 # isolates number
 temperaturelist_strip(temperature_list_temp)
 
 print("The average is "+ str(average(temperature_list)) + " degrees Farenheit.")
+print("It is also " + str(tempConvert('c', average(temperature_list))) + " degrees Celsius.")
